24064cdac0751a3dfa05fabed2575c9edb87279a
alpesh yadav <alpeshyadav2518@gmail.com>
2020-08-18 14:03:50 +0530

    desktop app done

--- Assets/Scripts/elementHandler.cs
+++ Assets/Scripts/elementHandler.cs
@@ -1,59 +1,4 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// /* Copyright (C) Xenfinity LLC - All Rights Reserved
-//  * Unauthorized copying of this file, via any medium is strictly prohibited
-//  * Proprietary and confidential
-//  * Written by Bilal Itani <bilalitani1@gmail.com>, June 2017
-//  */
-
-// public class elementHandler : MonoBehaviour {
-
-//     public float force = 5;
-
-//     Camera cam;
-
-// void Start(){
-//     cam = GetComponent<Camera>();
-// }
-
-//     private void Update()
-//     {
-//         if (Input.GetMouseButtonDown(0))
-//         {
-//             RaycastHit hit;
-//             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-//             if (Physics.Raycast(ray, out hit, 100.0f))
-//             {
-//                 if (hit.transform != null)
-//                 {
-
-//                     Rigidbody rb;
-
-//                     if (rb = hit.transform.GetComponent<Rigidbody>())
-//                     {
-//                         PrintName(hit.transform.gameObject);
-//                         LaunchIntoAir(rb);
-//                     }
-//                 }
-//             }
-//         }
-//     }
-
-//     private void PrintName(GameObject go)
-//     {
-//         print(go.name);
-//     }
-
-//     private void LaunchIntoAir(Rigidbody rig)
-//     {
-//         rig.AddForce(rig.transform.up * force, ForceMode.Impulse);
-//     }
-
-// }
-
+﻿
 using System.Collections;
  using System.Collections.Generic;
  using UnityEngine;
@@ -88,6 +33,12 @@ using System.Collections;
          var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
          RaycastHit Hit;
 
+
+        if (Input.GetKey ("escape")) {
+                 Application.Quit();
+        }
+
+
          if (Input.GetMouseButtonDown(0))
          {
              if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
@@ -104,7 +55,7 @@ using System.Collections;
                         Vector3 currPos = new Vector3(-17, 6, 30);
                         Vector3 newPos = new Vector3(-17, 6, 61);
                         // periodicTable.transform.position = Vector3.Lerp(currPos, newPos, Time.deltaTime * 20);
-                        periodicTable.transform.position = new Vector3(-17, 6, 61);
+                        periodicTable.transform.position = new Vector3(-17, 6, 1000);
 
                         instance = Instantiate(Resources.Load(element, typeof(GameObject))) as GameObject;
                         // GameObject button = (GameObject) Instantiate( buttonPrefab ) ;
@@ -142,3 +93,103 @@ using System.Collections;
          }
      }
  }
+
+
+// 0.66
+// 3.0565
+// 0.12
+// 0
+// -185.942
+// 0
+// 48
+// 48
+// 48
+
+// -1.66
+// 3.05
+// -0.13
+// 0
+// -185.942
+// 0
+// 48
+// 48
+// 48
+
+
+// 0.26
+// 3.25
+// 0.08
+// 0
+// -185.942
+// 0
+// 48
+// 48
+// 48
+
+// 2
+// -2.56
+// 2.808
+// -0.22
+// 0
+// -185.942
+// 0
+// 48
+// 48
+// 48
+
+
+
+// 3
+// 3.08
+// 0.35
+// 0.38
+// 0
+// -185.942
+// 0
+// 48
+// 48
+// 48
+
+
+// 4
+// 3.07
+// 3.02
+// 0.37
+// 0
+// -185.942
+// 0
+// 48
+// 48
+// 48
+
+// 5
+// -1.62
+// 0.35
+// 0
+// 0
+// -185.942
+// 0
+// 48
+// 48
+
+
+// -0.1
+// -1.5
+// 6.6
+
+// -0.65
+// -1.63
+// 5.1
+
+
+
+// 6
+// -2.01
+// 0.35
+// -0.05
+// 0
+// -185.942
+// 0
+// 48
+// 48
+// 48
